e4b9a7bbd9d153e6226099763a87f0f00d3413fa
Farsheed Ashouri <rodmena@me.com>
2019-05-16 02:16:49 +0430

    fixed part 2 of 3.

--- 10-days-of-statistics/day0.c
+++ 10-days-of-statistics/day0.c
@@ -9,6 +9,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+int comp(const void *elem1, const void *elem2) {
+  //
+  const int *ca = elem1;
+  const int *cb = elem2;
+  return (*ca > *cb) - (*ca < *cb);
+}
+
 int main() {
 
   // let's initialize vars
@@ -33,9 +40,32 @@ int main() {
     scanf("%d", &numbers[i]);
     sum += numbers[i];
   };
-
+  // ----------------------------------------------------------------------
   // Based on the problem, the first line is Mean:
   mean = sum / (float)n;
-  printf("%.1f", mean);
+  printf("%.1f\n", mean);
+  // ----------------------------------------------------------------------
+  // Second line is the median.  For this, first we need to sort the data to
+  // increasing or decreasing order
+  // I will use qsort() for this, it gets array, number of elements, size of
+  // each element and finally a comparison funciton. Comparison function will
+  // get pointer to two numbers:
+  // and of-course qsort will magicly sort the data:
+  qsort(numbers, n, sizeof(*numbers), comp);
+  // ----------------------------------------------------------------------
+  // now that we have sorted numbers, let's see if the number count is odd or
+  // even. if it's odd, we'll pick the middle number, else, we'll pick average
+  // of two, so:
+  float median;
+  int middle_element = n / 2;
+  if (n % 2 == 0) {
+    // it's even
+    median = (numbers[middle_element] + numbers[middle_element - 1]) / 2.0;
+
+  } else {
+    median = numbers[middle_element];
+  }
+  printf("%.1f\n", median);
+
   return EXIT_SUCCESS;
 }
